fe670d8b6fdd42f6cd74b781c935ecf495707144
lindsfitz <lindsaytfitz@gmail.com>
2021-11-02 11:11:38 -0700

    random updates

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,17 +1,17 @@
 const addPage = document.querySelector('#confirmPageAdd')
 const addList = document.querySelector('#newList')
 const pageArchive = document.querySelector('pageArchive');
-const includeSchedule = document.querySelector('#addSchedule').value()
-const includeToDo = document.querySelector('#addToDo').value()
-const includeInspo = document.querySelector('#addInspo').value()
-const includeDump = document.querySelector('#addDump').value()
+// const includeSchedule = document.querySelector('#addSchedule').value()
+// const includeToDo = document.querySelector('#addToDo').value()
+// const includeInspo = document.querySelector('#addInspo').value()
+// const includeDump = document.querySelector('#addDump').value()
+
+// Handlebars.registerPartial('schedulerPartial')
 
 
-function pageAdd() {
-    preventDefault();
+addPage.addEventListener('click', (e) => {
+  document.location.replace('/note')
+  
+})
 
-
-
-}
-
-addPage.addEventListener('click', pageAdd)
+// addPage.addEventListener('click', pageAdd)
