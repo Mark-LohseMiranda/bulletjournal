cf8f42d2e384aad0a3c3e54b79fe89bd8a0ff4e8
Kenneth Felix <kennf03@gmail.com>
2021-11-03 14:19:38 -0400

    fixed update

--- public/js/update.js
+++ public/js/update.js
@@ -15,6 +15,7 @@ const timeNineteen = document.querySelector('#timeNineteen');
 const braindumpText = document.querySelector('#braindumpText');
 const moment = require('moment');
 const toDoValue = document.querySelector('.toDoValue');
+
 saveBtn.addEventListener('click',(event) => {
     event.preventDefault();
     //update schedule
@@ -49,7 +50,21 @@ saveBtn.addEventListener('click',(event) => {
     }).catch(err=>console.log(err))
     // update todos
     // multiple tables or multiple content?
-    
+    const content1 = toDoValue[0].value() || " ";
+    const content2 = toDoValue[1].value() || " ";
+    const content3 = toDoValue[2].value() || " ";
+    const content4 = toDoValue[3].value() || " ";
+    const content5 = toDoValue[4].value() || " ";
+    const content6 = toDoValue[5].value() || " ";
+    const content7 = toDoValue[6].value() || " ";
+    const content8 = toDoValue[7].value() || " ";
+    const content9 = toDoValue[8].value() || " ";
+    const content10 = toDoValue[9].value() || " ";
+    fetch('/api/todos', {
+        method:'PUT',
+        body: JSON.stringify({content1,content2,content3,content4,content5,content6,content7,content8,content9,content10,day}),
+        headers:{'Content-Type' : 'application/json'},
+    }).catch(err=>console.log(err))
 })
 
 
