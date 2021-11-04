96b6749a8106efc57beca1a907b41b3040d0d9ec
Mark L-M <mark.lohsemiranda@gmail.com>
2021-11-03 17:23:57 -0700

    Update update.js

--- public/js/update.js
+++ public/js/update.js
@@ -27,7 +27,7 @@ const id = window.location.toString().split('/')[
 //deletes the currently displayed note
 
 deleteBtn.addEventListener('click',(event)=>{
-    let result = confirm("Are you sure you wnat to delete this note?");
+    let result = confirm("Are you sure you want to delete this note?");
     if (result) {
         fetch(`/api/notes/${id}`,{
             method:"DELETE"
@@ -41,82 +41,83 @@ deleteBtn.addEventListener('click',(event)=>{
     }
 })
 
-saveBtn.addEventListener('click',(event) => {
+saveBtn.addEventListener('click',async (event) => {
     event.preventDefault();
     //update schedule
     // const day = moment().format('D');
-    fetch(path(`/note/${id}`), {
+    console.log('before fetch')
+    await fetch(`/api/notes/3`, {
         method: 'GET',
         headers:{'Content-Type' : 'application/json'},
     }).then(response => response.json())
-    .then(data => {
-        const scheduleId = data.schedules.id
-        const braindumpId = data.braindumps.id        
-        const todoId = data.todos.id
-        const inspoId = data.inspirations.id;
+    .then(async data => {
+        console.log(data)
+        const scheduleId = data[0].schedules[0].id
+        const braindumpId = data[0].braindumps[0].id        
+        const todoId = data[0].todos[0].id
+        const inspoId = data[0].inspirations[0].id;
         
         const contentData = {
-            day:day,
-            content8am : timeEight.value(),
-            content9am : timeNine.value(),
-            content10am : timeTen.value(),
-            content11am : timeEleven.value(),
-            content12pm : timeTwelve.value(),
-            content1pm : timeThirteen.value(),
-            content2pm : timeFourteen.value(),
-            content3pm : timeFifteen.value(),
-            content4pm : timeSixteen.value(),
-            content5pm : timeSeventeen.value(),
-            content6pm : timeEighteen.value(),
-            content7pm : timeNineteen.value(),
-            content8pm : timeTwenty.value(),
+            content8am : timeEight.value,
+            content9am : timeNine.value,
+            content10am : timeTen.value,
+            content11am : timeEleven.value,
+            content12pm : timeTwelve.value,
+            content1pm : timeThirteen.value,
+            content2pm : timeFourteen.value,
+            content3pm : timeFifteen.value,
+            content4pm : timeSixteen.value,
+            content5pm : timeSeventeen.value,
+            content6pm : timeEighteen.value,
+            content7pm : timeNineteen.value,
+            content8pm : timeTwenty.value,
         }
-        fetch(join('/api/schedules/',scheduleId), {
+        fetch(`/api/schedules/${scheduleId}`, {
             method:'PUT',
             body: JSON.stringify(contentData),
             headers:{'Content-Type' : 'application/json'},
         }).catch(err => console.log(err))
         // update braindump
         let content = braindumpText.value();
-        fetch(join('/api/braindumps/',braindumpId), {
+        fetch(`/api/braindumps/${braindumpId}`, {
             method:'PUT',
             body: JSON.stringify({content,day}),
             headers:{'Content-Type' : 'application/json'},
         }).catch(err=>console.log(err))
         // update todos
         // multiple tables or multiple content?
-        let content1 = toDoValue[0].value() || " ";
-        let content2 = toDoValue[1].value() || " ";
-        let content3 = toDoValue[2].value() || " ";
-        let content4 = toDoValue[3].value() || " ";
-        let content5 = toDoValue[4].value() || " ";
-        let content6 = toDoValue[5].value() || " ";
-        let content7 = toDoValue[6].value() || " ";
-        let content8 = toDoValue[7].value() || " ";
-        let content9 = toDoValue[8].value() || " ";
-        let content10 = toDoValue[9].value() || " ";
-        fetch(join('/api/todos/',todoId), {
+        const content1 = toDoValue[0].value || " ";
+        const content2 = toDoValue[1].value || " ";
+        const content3 = toDoValue[2].value || " ";
+        const content4 = toDoValue[3].value || " ";
+        const content5 = toDoValue[4].value || " ";
+        const content6 = toDoValue[5].value || " ";
+        const content7 = toDoValue[6].value || " ";
+        const content8 = toDoValue[7].value || " ";
+        const content9 = toDoValue[8].value || " ";
+        const content10 = toDoValue[9].value || " ";
+        fetch(`/api/todos/${todoId}`, {
             method:'PUT',
             body: JSON.stringify({content1,content2,content3,content4,content5,content6,content7,content8,content9,content10,day}),
             headers:{'Content-Type' : 'application/json'},
         }).catch(err=>console.log(err));
         content = randomQuote.value();
-        fetch(join('/api/inspirations/',inspoId), {
+        fetch(`/api/inspirations/${inspoId}`, {
             method: 'PUT',
             body:JSON.stringify({content}),
             headers:{'Content-Type' : 'application/json'},
         }).catch(err=>console.log(err));
-        content1 = goalValue[0].value() || " ";
-        content2 = goalValue[1].value() || " ";
-        content3 = goalValue[2].value() || " ";
-        content4 = goalValue[3].value() || " ";
-        content5 = goalValue[4].value() || " ";
-        content6 = goalValue[5].value() || " ";
-        content7 = goalValue[6].value() || " ";
-        content8 = goalValue[7].value() || " ";
-        content9 = goalValue[8].value() || " ";
-        content10 = goalValue[9].value() || " ";
-        fetch(join('/api/goals/',todoId), {
+        content1 = goalValue[0].value || " ";
+        content2 = goalValue[1].value || " ";
+        content3 = goalValue[2].value || " ";
+        content4 = goalValue[3].value || " ";
+        content5 = goalValue[4].value || " ";
+        content6 = goalValue[5].value || " ";
+        content7 = goalValue[6].value || " ";
+        content8 = goalValue[7].value || " ";
+        content9 = goalValue[8].value || " ";
+        content10 = goalValue[9].value || " ";
+        fetch(`/api/goals/${todoId}`, {
             method:'PUT',
             body: JSON.stringify({content1,content2,content3,content4,content5,content6,content7,content8,content9,content10,day}),
             headers:{'Content-Type' : 'application/json'},
